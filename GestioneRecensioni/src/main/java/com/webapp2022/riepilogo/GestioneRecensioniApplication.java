4a2abc372c7e13145b2e3f6b0de21f59d415de72
Kristian <kristian.reale@gmail.com>
2022-11-23 16:45:42 +0100

    First commit

---
+++ GestioneRecensioni/src/main/java/com/webapp2022/riepilogo/GestioneRecensioniApplication.java
@@ -0,0 +1,14 @@
+package com.webapp2022.riepilogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@SpringBootApplication
+public class GestioneRecensioniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneRecensioniApplication.class, args);
+	}
+
+}
---
+++ GestioneRecensioni/src/test/java/com/master/webapp/GestioneRecensioniApplicationTests.java
@@ -0,0 +1,12 @@
+package com.master.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneRecensioniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
